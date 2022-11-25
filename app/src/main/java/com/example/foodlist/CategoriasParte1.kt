680a6821f2b50d329f5dd3a8fceb9778021fb786
Bibi Virguez <bbvirguez@gmail.com>
2022-11-24 19:55:55 -0500

    Subu el modulo de carrito y el de categorias

---
+++ app/src/main/java/com/example/foodlist/CategoriasParte1.kt
@@ -0,0 +1,11 @@
+package com.example.foodlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoriasParte1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categorias_parte1)
+    }
+}
